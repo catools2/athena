f936d011d27b3651af33108e7ce488be95559033
akeshmiri <akeshmiri.work@gmail.com>
2024-02-03 18:37:13 -0500

    small cleanup

--- athena-api-boot/src/main/java/org/catools/athena/rest/git/repository/CommitMetadataRepository.java
+++ athena-api-boot/src/main/java/org/catools/athena/rest/git/repository/CommitMetadataRepository.java
@@ -1,15 +1,12 @@
 package org.catools.athena.rest.git.repository;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.catools.athena.rest.core.repository.MetadataRepository;
 import org.catools.athena.rest.git.model.CommitMetadata;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Hidden
 @Transactional
-public interface CommitMetadataRepository extends JpaRepository<CommitMetadata, Long> {
-  Optional<CommitMetadata> findByNameAndValue(String name, String value);
+public interface CommitMetadataRepository extends MetadataRepository<CommitMetadata> {
 
 }
--- athena-api-boot/src/main/java/org/catools/athena/rest/git/service/CommitServiceImpl.java
+++ athena-api-boot/src/main/java/org/catools/athena/rest/git/service/CommitServiceImpl.java
@@ -4,20 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.catools.athena.git.model.CommitDto;
 import org.catools.athena.rest.git.mapper.GitMapper;
 import org.catools.athena.rest.git.model.Commit;
+import org.catools.athena.rest.git.repository.CommitMetadataRepository;
 import org.catools.athena.rest.git.repository.CommitRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+import static org.catools.athena.rest.core.utils.MetadataPersistentHelper.normalizeMetadata;
+
 @Service
 @RequiredArgsConstructor
 public class CommitServiceImpl implements CommitService {
+  private final CommitMetadataRepository commitMetadataRepository;
   private final CommitRepository commitRepository;
   private final GitMapper gitMapper;
 
   @Override
   public CommitDto save(CommitDto entity) {
     final Commit entityToSave = gitMapper.commitDtoToCommit(entity);
+    entityToSave.setMetadata(normalizeMetadata(entityToSave.getMetadata(), commitMetadataRepository));
     final Commit savedEntity = commitRepository.saveAndFlush(entityToSave);
     return gitMapper.commitToCommitDto(savedEntity);
   }
