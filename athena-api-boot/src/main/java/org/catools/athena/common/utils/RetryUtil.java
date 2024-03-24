4da4bd80b4e01f072f122a52bca07444c7edd769
akeshmiri <akeshmiri.work@gmail.com>
2024-03-24 08:06:31 -0400

    resolve some code smells

--- athena-api-boot/src/main/java/org/catools/athena/common/utils/RetryUtil.java
+++ athena-api-boot/src/main/java/org/catools/athena/common/utils/RetryUtil.java
@@ -2,33 +2,38 @@ package org.catools.athena.common.utils;
 
 import lombok.experimental.UtilityClass;
 
-import java.util.function.Function;
+import java.util.function.IntFunction;
 
 @UtilityClass
 public class RetryUtil {
-  public static <R> R retry(int retryCount, int interval, Function<Integer, R> m) {
-    Throwable ex = null;
+  public static <R> R retry(int retryCount, int interval, IntFunction<R> m) {
+    Exception ex = null;
     int counter = 0;
 
     while (retryCount-- > 0) {
       try {
         counter++;
         return m.apply(counter);
-      } catch (Throwable e) {
+      } catch (Exception e) {
         ex = e;
       }
 
-      try {
-        Thread.sleep(interval);
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
+      sleep(interval);
     }
 
-    if (ex instanceof RuntimeException) {
-      throw (RuntimeException) ex;
+    if (ex instanceof RuntimeException rex) {
+      throw rex;
     }
     throw new RuntimeException(ex);
 
+  }
+
+  @SuppressWarnings({"java:S2142", "java:S112"})
+  private static void sleep(int interval) {
+    try {
+      Thread.sleep(interval);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
