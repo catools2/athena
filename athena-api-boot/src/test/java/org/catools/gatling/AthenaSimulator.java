63694e9d500113dce1f2d1e8df08ada161d5321c
akeshmiri <akeshmiri.work@gmail.com>
2024-05-24 18:39:20 -0400

    add test and cleanup code

--- athena-api-boot/src/test/java/org/catools/gatling/AthenaSimulator.java
+++ athena-api-boot/src/test/java/org/catools/gatling/AthenaSimulator.java
@@ -4,8 +4,10 @@ import io.gatling.javaapi.core.Assertion;
 import io.gatling.javaapi.core.PopulationBuilder;
 import io.gatling.javaapi.core.Simulation;
 import org.catools.gatling.population.common.PopulationInfo;
-import org.catools.gatling.population.git.CommitPopulation;
-import org.catools.gatling.population.git.GitRepositoryPopulation;
+import org.catools.gatling.population.core.EnvironmentPopulation;
+import org.catools.gatling.population.core.ProjectPopulation;
+import org.catools.gatling.population.core.UserPopulation;
+import org.catools.gatling.population.core.VersionPopulation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +17,10 @@ import static io.gatling.javaapi.http.HttpDsl.http;
 public class AthenaSimulator extends Simulation {
   private static List<PopulationInfo> getPopulationInfo() {
     List<PopulationInfo> populationsInfo = new ArrayList<>();
-    populationsInfo.addAll(CommitPopulation.getPopulationsInfo());
-    populationsInfo.addAll(GitRepositoryPopulation.getPopulationsInfo());
+    populationsInfo.addAll(ProjectPopulation.getPopulationsInfo());
+    populationsInfo.addAll(EnvironmentPopulation.getPopulationsInfo());
+    populationsInfo.addAll(UserPopulation.getPopulationsInfo());
+    populationsInfo.addAll(VersionPopulation.getPopulationsInfo());
     return populationsInfo;
   }
 
