b62d384a899810df0100af78c3dd387b37619087
akeshmiri <akeshmiri.work@gmail.com>
2026-09-07 18:21:56 -0500

    fix helm

--- orchestration/athena/preinstall.sh
+++ orchestration/athena/preinstall.sh
@@ -2,7 +2,8 @@
 
 cd ../../
 ./mvnw org.codehaus.mojo:versions-maven-plugin:2.15.0:set-property -Dproperty=revision -DnewVersion=2.0.0 -q
-./mvnw clean package docker:build -DskipTests -T 2C
+./mvnw clean package -DskipTests -T 2C
+./mvnw -pl athena-boot-core,athena-boot-git,athena-boot-kube,athena-boot-metric,athena-boot-pipeline,athena-boot-spec,athena-boot-tms,athena-gateway docker:build -DskipTests -T 2C
 
 cd orchestration/athena
 
